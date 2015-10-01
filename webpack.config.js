741da995dd0a383a9e5d81f2bee1a1dfbc2a625d
Dan Jacob <danjac354@gmail.com>
2015-10-01 19:08:28 +0300

    Combine webpack configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,44 @@
 var path = require('path');
 var webpack = require('webpack');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
+
+var env = process.env.WEBPACK_ENV;
+
+var jsLoaders = ['babel?stage=0&optional[]=runtime'];
+
+var entry = ['./js/main.js'];
+
+var plugins = [
+  new ExtractTextPlugin('[name].css', {
+    allChunks: true
+  })
+];
+
+switch(process.env.WEBPACK_ENV) {
+  case 'dev':
+  jsLoaders.unshift('react-hot');
+  entry.concat([
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/only-dev-server',
+  ]);
+  plugins.unshift(new webpack.HotModuleReplacementPlugin());
+  break;
+  case 'prod':
+  plugins.push(new UglifyJsPlugin({ minimize: true }));
+  break;
+}
 
 module.exports = {
   context: path.join(__dirname, 'ui'),
-  devtool: 'eval',
-  entry: [
-    'webpack-dev-server/client?http://localhost:8080',
-    'webpack/hot/only-dev-server',
-    './js/main.js'
-  ],
+  devtool: 'source-map',
+  entry: entry,
   output: {
     path: path.join(__dirname, 'dist'),
     filename: "[name].js",
     publicPath: '/static/'
   },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new ExtractTextPlugin('[name].css', {
-      allChunks: true
-    })
-  ],
+  plugins: plugins,
   module: {
     loaders: [
       {
@@ -33,7 +51,7 @@ module.exports = {
       },
       {
         test: /\.(js|jsx)$/,
-        loaders: ['react-hot', 'babel?stage=0&optional[]=runtime'],
+        loaders: jsLoaders,
         include: path.join(__dirname, 'ui/js'),
         exclude: path.join(__dirname, 'node_modules')
       }
--- webpack.prod.config.js
+++
@@ -1,6 +0,0 @@
-var config = require('./webpack.config');
-
-config.entry = ['./js/main.js'];
-
-module.exports = config;
-
