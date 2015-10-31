b9baafdcc030ae483ec32b1862e1a4676a6e4b4f
Dan Jacob <danjac354@gmail.com>
2015-10-31 08:09:02 +0200

    Adding a test

---
+++ server/server_test.go
@@ -0,0 +1,60 @@
+package server
+
+import (
+	"github.com/Sirupsen/logrus"
+	"github.com/danjac/random_movies/models"
+	"github.com/unrolled/render"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func newTestServer() *Server {
+	return &Server{
+		DB:     &MockDB{},
+		OMDB:   &MockOMDB{},
+		Render: render.New(),
+		Log:    logrus.New(),
+		Config: &Config{},
+	}
+}
+
+type MockOMDB struct{}
+
+func (o *MockOMDB) Find(title string) (*models.Movie, error) {
+	return testMovie, nil
+}
+
+type MockDB struct{}
+
+var testMovie = &models.Movie{}
+
+func (db *MockDB) GetAll() ([]*models.Movie, error) {
+	return []*models.Movie{testMovie}, nil
+}
+
+func (db *MockDB) GetRandom() (*models.Movie, error) {
+	return testMovie, nil
+}
+
+func (db *MockDB) Get(imdbID string) (*models.Movie, error) {
+	return testMovie, nil
+}
+
+func (db *MockDB) Save(movie *models.Movie) error {
+	return nil
+}
+
+func (db *MockDB) Delete(imdbID string) error {
+	return nil
+}
+
+func TestRandomMovie(t *testing.T) {
+	req, _ := http.NewRequest("GET", "", nil)
+	w := httptest.NewRecorder()
+	s := newTestServer()
+	s.getRandomMovie(w, req)
+	if w.Code != http.StatusOK {
+		t.Errorf("Random movie did not return %v", http.StatusOK)
+	}
+}
