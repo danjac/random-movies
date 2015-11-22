e2e201d6994b938c9eee34bd0671828cf3df1543
Dan Jacob <danjac354@gmail.com>
2015-11-22 02:51:14 +0200

    Babel 6 upgrade

--- webpack.config.js
+++ webpack.config.js
@@ -5,9 +5,7 @@ var UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
 
 var env = process.env.WEBPACK_ENV;
 
-var jsLoaders = ['babel?stage=0&optional[]=runtime'];
-
-var entry = ['./js/main.js'];
+var entry = ['babel-polyfill', './js/main.js'];
 
 var plugins = [
   new ExtractTextPlugin('[name].css', {
@@ -17,7 +15,6 @@ var plugins = [
 
 switch(process.env.WEBPACK_ENV) {
   case 'dev':
-  jsLoaders.unshift('react-hot');
   entry.unshift('webpack-dev-server/client?http://localhost:8080');
   entry.unshift('webpack/hot/only-dev-server');
   plugins.unshift(new webpack.HotModuleReplacementPlugin());
@@ -48,8 +45,11 @@ module.exports = {
         loader: 'url-loader?limit=200000'
       },
       {
-        test: /\.(js|jsx)$/,
-        loaders: jsLoaders,
+        test: /\.js$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015', 'stage-0']
+        },
         include: path.join(__dirname, 'ui/js'),
         exclude: path.join(__dirname, 'node_modules')
       }
