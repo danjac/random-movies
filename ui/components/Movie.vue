065363fff6d8b10cb438653c9e0ebb682574b1f3
Dan Jacob <danjac354@gmail.com>
2015-11-26 23:01:18 +0200

    Render movie

--- ui/components/Movie.vue
+++ ui/components/Movie.vue
@@ -1,3 +1,62 @@
 <template>
-  Movie goes here...
+  <div class="row">
+
+    <div class="col-md-3">
+        <img class="img-responsive" 
+             v-bind:src="movie.Poster" 
+             v-bind:alt="movie.Title" />
+    </div>
+
+    <div class="col-md-9">
+        <h2>{{movie.Title}}</h2> 
+        <dl class="dl-unstyled">
+            <dt>Year</dt>
+            <dd>{{movie.Year}}</dd>
+            <dt>Actors</dt>
+            <dd>{{movie.Actors}}</dd>
+            <dt>Director</dt>
+            <dd>{{movie.Director}}</dd>
+          </dl>
+          <p class="well">{{movie.Plot}}</p>
+          <div class="button-group">
+            <button v-on:click="getRandom" class="btn btn-primary">
+                <span class="glyphicon glyphicon-random"></span> Random
+            </button>
+          </div>
+    </div>
+  </div>
+
 </template>
+
+<script>
+export default {
+    
+    name: "Movie",
+    data() {
+        return {
+            movie: {}
+        };
+    },
+    route: {
+        data({ to }) {
+            const id = to.params.id;
+            if (id) {
+            } else {
+                return this.$http.get("/api/", movie => {
+                    return {
+                        movie
+                    };
+                });
+            }
+        }
+    },
+    methods: {
+        getRandom(event) {
+            console.log("getrandom!!!")
+            this.$http.get("/api/", movie => { this.movie = movie; });
+        }
+    }
+};
+    
+</script>
+
--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue';
+import Resource from 'vue-resource';
 import Router from 'vue-router';
 
 import App from './components/App.vue';
 import Movie from './components/Movie.vue';
 
 Vue.use(Router);
+Vue.use(Resource);
 
 const router = new Router();
 
